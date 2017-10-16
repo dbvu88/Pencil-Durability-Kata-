09bf39c97e163ec7f12051502e04b2341ba22d8a
Duc Vu <de.yd315@gmail.com>
2017-10-16 00:32:48 -0500

    reworking of erase method

--- pencil.rb
+++ pencil.rb
@@ -1,40 +1,135 @@
-class Pencil
-  attr_reader :pointDurability, :length, :filePath
+class MissingFilePathError < StandardError
+end
 
-  def initialize(durability, length, path)
+class TextNotErasableError < StandardError
+end
+
+class TextNotFoundError < StandardError
+end
+
+class PencilNotUsable < StandardError
+end
+
+class EraserNotUsable < StandardError
+end
+
+class Pencil
+
+  attr_reader :pointDurability, :length, :filePath, :pointDegradation, :eraserDurability, :writable, :erasable
+
+  def initialize(durability, eraser, length, path)
+    if path == nil || path.empty?
+      raise MissingFilePathError
+    end
+
+    @eraserDurability = eraser
+
+    @writable = true
+    @erasable = true
+
     @pointDurability = durability
+    @pointDegradation = 0
     @length = length
     @filePath = path
-    File.open(@filePath, 'w') do |f|
-      f.print ''
-    end
-    
+
   end
 
   def write(input)
-    # input = gets
+    if @length == 0
+      raise PencilNotUsable
+    end
     text = ""
     input.each_char do |chr|
-      if chr == chr.upcase
-        if @pointDurability >= 2 && !chr.chomp.strip.empty?
-          @pointDurability -= 2
+      if chr != " "
+        if chr == chr.upcase && @pointDegradation <= @pointDurability-2
+          @pointDegradation += 2
+          text += chr
+        elsif chr != chr.upcase && @pointDegradation <= @pointDurability-1
+          @pointDegradation += 1
           text += chr
         else
           text += " "
         end
       else
-        if @pointDurability >= 1 && !chr.chomp.strip.empty?
-          @pointDurability -= 1
-          text += chr
-        else
-          text += " "
-        end
+        text += " "
       end
     end
 
     File.open(@filePath, 'a') do |f|
       f.print text
     end
+  end
+
+  def sharpen
+    if @length == 0
+      raise PencilNotUsable
+    end
+    if @pointDegradation == @pointDurability
+      @pointDegradation = 0
+      @length -= 1
+    else
+      # puts "Your pencil is still sharp enough."
+    end
+  end
+
+
+  def erase(input)
+    input = input.strip
+
+    if @eraserDurability == 0
+      raise EraserNotUsable
+    end
+
+    text = ""
+
+    File.open(@filePath, 'r') do |f|
+      text = f.read
+    end
+
+
+    if input.size > text.size
+      raise TextNotErasableError
+    end
+
+    if !text.include? input
+      raise TextNotFoundError
+    end
+
+    indexLastOccurence = 0
+    scannedTextTotal = ""
+    scannedText = ""
+    # initial unscannedText is the entire text from the file
+    unscannedText = text
+    # search for the last occurence of input in the text
+    while unscannedText.include? input do
+      # get the index of the first occurence
+      i = unscannedText.index(input)
+      
+      # assign the scanned text & unscanned text to approriate varible
+      scannedText = unscannedText[0...i+input.size]
+
+      scannedTextTotal << scannedText
+      unscannedText = unscannedText[i+input.size...text.size]
+    end
+
+    newText = scannedTextTotal[0, scannedTextTotal.size - input.size] << " " * input.size << unscannedText
+
+    File.open(@filePath, 'w') do |f|
+      f.print newText
+    end
+    @eraserDurability -= input.gsub(/\s/,"").split("").size
+  end
+
+  def edit(input)
+
+    File.open(@filePath, 'r') do |f|
+        text = f.read
+    end
+
+    text.index("   ")
+
+
+
   end
 
 end
--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -2,8 +2,11 @@ require_relative "../pencil"
 
 RSpec.describe Pencil do
 
-path = "SheetOfPaper.txt"
-myPencil = Pencil.new(25, 10, path)
+path = "SheetOfPaperTest.txt"
+File.open(path, 'w') do |f|
+  f.print ""
+end
+myPencil = Pencil.new(25, 100, 10, path)
   describe '#initialize' do
     context 'when creating a new pencil' do
       it 'returns a new pecil with given point durability' do
@@ -12,34 +15,89 @@ myPencil = Pencil.new(25, 10, path)
       it 'returns a new pecil with given point durability' do
         expect(myPencil.length).to eq 10
       end
+      it 'returns a new pecil with given eraser durability' do
+        expect(myPencil.eraserDurability).to eq 100
+      end
       it 'returns a blank file' do
         expect(File.read(path).strip).to eq ""
       end
+      it "raises error when file not found" do
+        expect{raise Pencil.new(10, 100, 2,"")}.to raise_error(MissingFilePathError)
+      end
     end
-
-    # context 'when the plane is not flying' do
-    #   it 'returns false' do
-    #     expect(Airplane.new.flying?).to eq false
-    #   end
-    # end
   end
 
   describe '#write' do
     context 'when writing text to the SheetOfPaper.txt file with the pencil' do
       it 'degrades the point durability' do
         myPencil.write("Hi")
-        expect(myPencil.pointDurability).to eq 22
+        expect(myPencil.pointDegradation).to eq 3
         myPencil.write(" I am a blank piece of paper")
-        expect(myPencil.pointDurability).to eq 0
+        expect(myPencil.pointDegradation).to eq 25
         myPencil.write(" Good Bye ")
-        expect(myPencil.pointDurability).to eq 0
+        expect(myPencil.pointDegradation).to eq 25
         expect(File.read(path).size).to eq 40
       end
       it 'returns a letter in exchange of point durability' do
-
         expect(File.read(path)).to eql "Hi I am a blank piece of paper          "
-
       end
     end
   end
+
+  describe '#sharpen' do
+    context 'when point degradation is zero' do
+      it 'reset the point degradation' do
+        myPencil.sharpen
+        expect(myPencil.pointDegradation).to eq 0
+        expect(myPencil.length).to eq 9
+      end
+    end
+    context 'when point degradation is not zero' do
+      it 'does not reset the point degradation' do
+        myPencil.write("After sharpening")
+        myPencil.sharpen
+        expect(myPencil.pointDegradation).to eq 16
+      end
+    end
+  end
+
+  describe '#erase' do
+    context 'when promting the pencil to erase text that not included in the file' do
+      it 'raises TextNotFoundError' do
+        expect{ raise myPencil.erase "Physician"}.to raise_error(TextNotFoundError)
+      end
+    end
+
+    context 'when asking to erase text from the file' do
+      it 'replaces text with whitespace and decreases eraserDurability' do
+        myPencil.erase("blank")
+        expect(File.read(path)).to eq "Hi I am a       piece of paper          After sharpening"
+        expect{ raise myPencil.erase("hi")}.to raise_error(TextNotFoundError)
+        myPencil.erase("Hi")
+        expect(File.read(path)).to eq "   I am a       piece of paper          After sharpening"
+        myPencil.erase("sharpening")
+        expect(File.read(path)).to eq "   I am a       piece of paper          After           "
+
+        myPencil.erase("e")
+        expect(File.read(path)).to eq "   I am a       piece of paper          Aft r           "
+        myPencil.erase("e")
+        expect(File.read(path)).to eq "   I am a       piece of pap r          Aft r           "
+        myPencil.write(" I am ready")
+        expect(File.read(path)).to eq "   I am a       piece of pap r          Aft r            I am ready"
+        myPencil.sharpen
+        expect(myPencil.pointDegradation).to eq 0
+        expect(myPencil.length).to eq 8
+        expect(myPencil.eraserDurability).to eq 81
+
+        myPencil.erase("I am  ")
+        expect(File.read(path)).to eq "   I am a       piece of pap r          Aft r                 ready"
+        expect(myPencil.eraserDurability).to eq 78
+      end
+    end
+
+
+  end
+
+
+
 end
